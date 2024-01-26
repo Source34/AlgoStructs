cf9fad20a338827d1514c199589cb198d689ed28
Source34 <rom.steam2010@yandex.ru>
2024-01-26 19:25:16 +0300

    Initial commit
    Created general solution structure

---
+++ Algorithms/Utils/SortUtils.cs
@@ -0,0 +1,45 @@
+﻿namespace Algorithms.Utils
+{
+    internal static class SortUtils
+    {
+        internal static bool IsGreater(int x, int y)
+        {
+            return x > y;
+        }
+
+        internal static bool IsLess(int x, int y)
+        {
+            return x < y;
+        }
+
+        internal static bool IsGreater(float x, float y)
+        {
+            return x > y;
+        }
+
+        internal static bool IsLess(float x, float y)
+        {
+            return x < y;
+        }
+
+        internal static bool IsGreater(double x, double y)
+        {
+            return x > y;
+        }
+
+        internal static bool IsLess(double x, double y)
+        {
+            return x < y;
+        }
+
+        internal static bool IsGreater(decimal x, decimal y)
+        {
+            return x > y;
+        }
+
+        internal static bool IsLess(decimal x, decimal y)
+        {
+            return x < y;
+        }
+    }
+}
