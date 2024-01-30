ca81061c6d482d0f575349b1f517805e1916bf56
Source34 <rom.steam2010@yandex.ru>
2024-01-30 04:58:47 +0300

    Add quick sort tests

---
+++ Algorithms/Sortings/BubbleSortExtension.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.Utils;
+
+namespace Algorithms.Sortings
+{
+    public static class BubbleSortExtension
+    {
+        public static IEnumerable<int> BubbleSort(this IEnumerable<int> data, SortDirection direction = SortDirection.Descending)
+        {
+            Compare compare = direction == SortDirection.Descending ? SortUtils.IsLess : SortUtils.IsGreater;
+
+            var arr = data.ToArray();
+            var length = arr.Length;
+
+            for (var i = 0; i < length - 1; i++)
+            for (var j = i; j < length - 1; j++)
+                if (compare(arr[i], arr[j + 1]))
+                    (arr[i], arr[j + 1]) = (arr[j + 1], arr[i]);
+
+            return arr;
+        }
+    }
+}
